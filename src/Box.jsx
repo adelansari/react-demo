f2cd9fad59a5df01905dd737154477331e207655
Adel Ansari <adelansari.a@gmail.com>
2024-03-25 14:54:44 +0200

    Update heading level in Box component

--- src/Box.jsx
+++ src/Box.jsx
@@ -2,7 +2,7 @@ import './Box.css';
 const Box = (props) => {
   return (
     <div className='Box'>
-      <h1>Name: {props.name}</h1>
+      <h2>Name: {props.name}</h2>
       <p>Title: {props.title}</p>
       <p>Location: {props.location}</p>
     </div>
