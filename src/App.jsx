a57ee071354fe6f9f5ffa97e1399feb0ed1aa7e9
Adel Ansari <adelansari.a@gmail.com>
2024-03-25 14:54:33 +0200

    Add counter and person list functionality to App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,43 @@
+import { useState } from 'react';
 import './App.css';
 import Box from './Box';
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  const [persons, setPerson] = useState([
+    { id: 1, name: 'Adel', title: 'cool', location: 'Helsinki' },
+    { id: 2, name: 'Toni', title: 'cooler', location: 'Helsinki' },
+    { id: 3, name: 'Margit', title: 'coolest', location: 'Helsinki' },
+  ]);
+
+  const clickIncHandler = () => {
+    setCounter(counter + 1);
+  };
+  const clickDecHandler = () => {
+    setCounter(counter - 1);
+  };
+  const clickResetHandler = () => {
+    setCounter(0);
+  };
+
   return (
     <>
       <h1 className='adel-header'>Hello. My name is Adel</h1>
 
-      <Box name='Adel' title='cool' location='Helsinki' />
-      <Box name='Toni' title='cooler' location='Helsinki' />
-      <Box name='Margit' title='coolest' location='Helsinki' />
+      <p>Counter: {counter}</p>
+      <button className='inc-counter' onClick={clickIncHandler}>
+        Increment!
+      </button>
+      <button className='dec-counter' onClick={clickDecHandler}>
+        Decrement!
+      </button>
+      <button className='reset-counter' onClick={clickResetHandler}>
+        Reset
+      </button>
+
+      {persons.map((person) => (
+        <Box key={person.id} name={person.name} title={person.title} location={person.location} />
+      ))}
     </>
   );
 }
